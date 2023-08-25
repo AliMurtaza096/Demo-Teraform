d041829e30a8e40b1db20a4b362bcade317ee48f
AliMurtaza096 <murtaza2019@namal.edu.pk>
2023-08-25 11:57:43 +0500

    Added JSON file

--- get_mac_addr.py
+++ get_mac_addr.py
@@ -11,10 +11,15 @@ import re
 
 def fetch_vm_network_info():
     all_mac_addresses = {}
+
+    #Setting Up the API 
     proxmox = ProxmoxAPI('192.168.122.109:8006', user='root@pam', password='@ali.1397', verify_ssl=False)
     
+    #API call for getting details of VMs
     vm_details = proxmox.nodes("ali").qemu.get()
     vm_ids = [vm_id['vmid'] for vm_id in vm_details if vm_id['vmid']!= 100 ]
+
+    #Getting MAC Address of  each VM
     for vm_id in vm_ids:
 
         config = proxmox.nodes("ali").qemu(vm_id).config.get()
@@ -23,6 +28,9 @@ def fetch_vm_network_info():
         mac_address  = virtio[1]
 
         all_mac_addresses[vm_id] = mac_address
+
+    #Storing the VMs MAC Addresses with Their VMs ids respectively to JSON file
+    #So we can use them later
     with open('ali_VMs_MAC_Adresses.json','w') as file: 
         json.dump(all_mac_addresses,file,indent=4)  
     print(json.dumps(all_mac_addresses))
